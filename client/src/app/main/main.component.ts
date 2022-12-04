22f756180eed52db1e02797ab61c4bed577030f9
nDavood <DAtamaNguyen@gmail.com>
2022-12-03 22:24:07 -0800

    early comit

--- client/src/app/main/main.component.ts
+++ client/src/app/main/main.component.ts
@@ -25,7 +25,6 @@ export class MainComponent implements OnInit {
   testTableInit() {
     this.http.get('https://movie-174project.herokuapp.com/testTable').subscribe(res => {
       this.testTableData = res;
-      console.log(this.testTableData);
     });
   }
 
