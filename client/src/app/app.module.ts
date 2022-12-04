50ef72df738462afb0342f71467996d5fc6c0743
nDavood <DAtamaNguyen@gmail.com>
2022-12-03 23:45:22 -0800

    PUSH

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
 import { MaterialModule } from './helpers/material.module';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,9 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/main/main.component.ts
+++ client/src/app/main/main.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
@@ -7,6 +8,8 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
+
   testTableColumns: string[] = ['fname','lname','age'];
   testTableData: any;
 
@@ -14,11 +17,20 @@ export class MainComponent implements OnInit {
   customerData: any;
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit(): void {
     console.log('MainComponent Initiation complete');
+
+    this.form = this.fb.group({
+      addressID: [null],
+      f_name: [null],
+      l_name: [null],
+      email: [null, [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      phoneNumber: [null]
+    });
 
     //Initalize SQL Tables that we are using
     this.testTableInit();
@@ -36,6 +48,12 @@ export class MainComponent implements OnInit {
     this.http.get('https://movie-174project.herokuapp.com/customerInfo').subscribe(res => {
       this.customerData = res;
     });
+  }
+
+  submitDetails(form: any) {
+    console.log('SUBMITTED');
+    this.http.post('', JSON.stringify(form.value, null, 4)).subscribe();
+    alert('ITS BROKEN!!\n' + JSON.stringify(form.value, null, 4));
   }
 
 }
